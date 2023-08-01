1afa71d5b997e22f0c829d5caf9f354d29379344
NibuTake <0815takeshi@gmail.com>
2023-08-01 21:39:34 +0900

    👍 add some functions

--- backend/domain/model.py
+++ backend/domain/model.py
@@ -11,7 +11,7 @@ class Customer:
 
 @dataclasses.dataclass
 class Product:
-    title: str
+    name: str
     price: int
 
 
@@ -21,10 +21,28 @@ class Casher:
     products: List[Product]
 
     def calculate_total_price(self) -> int:
-        return sum([product.price for product in self.products])
+        price = 0
+        for product in self.products:
+            price += product.price
+        return price
 
     def calculate_discount(self) -> int:
         if self.customer.is_registered:
+            return self.calculate_total_price() * 0.1
+        else:
+            return 0
+
+    # def calculate_discount(self) -> int:
+    #     return self._discount_by_register() + self._discount_by_age()
+
+    def _discount_by_register(self) -> int:
+        if self.customer.is_registered:
+            return self.calculate_total_price() * 0.1
+        else:
+            return 0
+
+    def _discount_by_age(self) -> int:
+        if self.customer.age >= 35:
             return self.calculate_total_price() * 0.05
         else:
             return 0
--- backend/domain/test_model.py
+++ backend/domain/test_model.py
@@ -1,3 +1,48 @@
-def test_sample():
-    True
+from model import Casher, Customer, Product
 
+
+class Test_Casher_合計金額:
+    def test_カゴの商品の合計金額を算出する(self):
+        # Given
+        customer = Customer("test", 30, False)
+        product_list = [Product("", 1000), Product("", 2000)]
+
+        # When
+        sut = Casher(customer, product_list)
+
+        # THen
+        assert sut.calculate_total_price() == 3000
+
+    def test_カゴに商品がない場合は0円(self):
+        customer = Customer("test", 30, False)
+        product_list = []
+
+        sut = Casher(customer, product_list)
+
+        assert sut.calculate_total_price() == 0
+
+
+class Test_Casher_減額:
+    def test_登録済み顧客の場合_10パー割引(self):
+        customer = Customer("test", 30, True)
+        product_list = [Product("", 1000)]
+
+        sut = Casher(customer, product_list)
+
+        assert sut.calculate_discount() == 100
+
+    def test_未登録の顧客の場合_割引なし(self):
+        customer = Customer("test", 30, False)
+        product_list = [Product("", 1000)]
+
+        sut = Casher(customer, product_list)
+
+        assert sut.calculate_discount() == 0
+
+    # def test_未登録の35歳以上の顧客の場合_5パー割引(self):
+    #     customer = Customer("test", 35, False)
+    #     product_list = [Product("", 1000)]
+
+    #     sut = Casher(customer, product_list)
+
+    #     assert sut.calculate_discount() == 50
